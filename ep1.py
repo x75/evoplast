5f0cf3d6158df7311dadc10df3939b91a96c9c5d
Oswald Berthold <opt@sdfk.de>
2016-01-14 09:55:49 +0100

    init

---
+++ ep1.py
@@ -0,0 +1,12 @@
+
+import tensorflow as tf
+import numpy as np
+import matplotlib.pylab as pl
+
+# small random recurrent network: 2:3:1
+
+x = tf.placeholder(tf.float32, [num_steps, batch_size, input_dim])
+
+# matmul etc
+
+# h = 
