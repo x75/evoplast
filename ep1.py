63999cb76824164abd5ba65739f5755f7bc98f54
Oswald Berthold <bertolos@informatik.hu-berlin.de>
2016-01-14 16:47:38 +0100

    - sketches

--- ep1.py
+++ ep1.py
@@ -5,8 +5,143 @@ import matplotlib.pylab as pl
 
 # small random recurrent network: 2:3:1
 
-x = tf.placeholder(tf.float32, [num_steps, batch_size, input_dim])
+# x = tf.placeholder(tf.float32, [num_steps, batch_size, input_dim])
 
 # matmul etc
 
 # h = 
+
+
+# pseudo
+
+
+# x (input)
+
+# h = tanh(W_h h + W_i x + b_h)
+
+# y = W_o h + b_o
+
+# W_h = V_h {x, h, y} + a_h
+
+# W_i = V_i {x, h, y} + a_i
+
+# W_o = V_o {x, h, y} + a_o
+
+# b_h = A_h {x, h, y} + a_b_h
+
+# b_o = A_o {x, h, y} + a_b_o
+
+
+num_steps = 44100
+input_dim = 1
+output_dim = input_dim
+N1 = 4
+N2 = 100
+
+
+# base network
+W1_h = np.random.normal(0., 0.65, (N1, N1))
+b1_h = np.random.normal(0., 0.3, (N1, 1))
+
+W1_i = np.random.normal(0., 0.3, (N1, input_dim))
+# b1_i = np.random.normal(0., 0.3, (N1, 1))
+
+W1_o = np.random.normal(0., 0.5, (output_dim, N1))
+b1_o = np.random.normal(0., 0.3, (1, 1))
+
+t_ = np.linspace(0, np.pi * 2 * (num_steps/1000), num_steps, endpoint=False)
+x_ = 0.5 * np.sin(t_ / 10.) + 0.5 * np.sin(t_ * 3)
+
+h_ = np.zeros((num_steps, N1))
+y_ = np.zeros((num_steps, output_dim))
+
+h = np.zeros((N1, 1))
+y = np.zeros((output_dim, 1))
+
+tau = 0.9
+
+# modulating network
+v_input_dim  = input_dim + N1 + output_dim
+v_output_dim = W1_h.size + W1_i.size + W1_o.size
+
+vx_ = np.zeros((num_steps, v_input_dim))
+
+vW_h = np.random.normal(0., 0.1, (N2, N2))
+vb_h = np.random.normal(0., 0.01, (N2, 1))
+
+vW_i = np.random.normal(0., 0.01, (N2, v_input_dim))
+
+vW_o = np.random.normal(0., 0.5, (v_output_dim, N2))
+vb_o = np.random.normal(0., 0.5, (v_output_dim, 1))
+
+vh_ = np.zeros((num_steps, N2))
+vy_ = np.zeros((num_steps, v_output_dim))
+
+vh_ = np.zeros((num_steps, N2))
+vy_ = np.zeros((num_steps, v_output_dim))
+
+vh = np.zeros((N2, 1))
+vy = np.zeros((v_output_dim, 1))
+
+vtau = 0.999
+
+# loop
+for i in xrange(num_steps):
+    # update base
+    x = x_[i]
+    h = tau * h + (1-tau) * np.tanh(np.dot(W1_h, h) + np.dot(W1_i, x) + b1_h)
+    h += np.random.normal(0, 0.001, h.shape)
+    y = np.dot(W1_o, h) + b1_o
+
+    # print h_[i].shape, h.shape
+    # print y_[i].shape, y.shape
+
+    # store    
+    h_[i] = h.copy().flatten()
+    y_[i] = y.copy().flatten()
+
+    # update modulator
+    vx = np.vstack((x, h, y))
+    vh = vtau * vh + (1 - vtau) * np.tanh(np.dot(vW_h, vh) + np.dot(vW_i, vx) + vb_h)
+    vh += np.random.normal(0, 0.001, vh.shape)
+    # vh = np.tanh(np.dot(vW_h, vh) + np.dot(vW_i, vx) + vb_h)
+    vy = np.dot(vW_o, vh) + vb_o
+
+    # store
+    vx_[i] = vx.copy().flatten()
+    vh_[i] = vh.copy().flatten()
+    vy_[i] = vy.copy().flatten()
+
+    # update base matrices
+    W1_i = vy[:W1_i.size].reshape(W1_i.shape)
+    W1_h = vy[W1_i.size:W1_i.size+W1_h.size].reshape(W1_h.shape)
+    W1_o = vy[W1_i.size+W1_h.size:W1_i.size+W1_h.size+W1_o.size].reshape(W1_o.shape)
+        
+    # print vy.shape
+
+# base
+pl.subplot(321)
+pl.title("input") 
+pl.plot(x_)
+pl.subplot(323)
+pl.title("hidden") 
+pl.plot(h_)
+pl.subplot(325)
+pl.title("output") 
+pl.plot(y_)
+# modulator
+pl.subplot(322)
+pl.title("vinput") 
+pl.plot(vx_)
+pl.subplot(324)
+pl.title("vhidden") 
+pl.plot(vh_)
+pl.subplot(326)
+pl.title("voutput") 
+pl.plot(vy_)
+pl.show()
+
+
+from scipy.io import wavfile
+wavdata = (y_.flatten()/np.max(np.abs(y_)) * 32767).astype(np.int16)
+wavfile.write("ep1.wav", 44100, wavdata)
---
+++ rnntests.py
@@ -0,0 +1,186 @@
+# this is from https://m.reddit.com/r/MachineLearning/comments/3sok8k/tensorflow_basic_rnn_example_with_variable_length/
+
+# just run an untrained random network
+# a) basics
+# b) test cwrnn
+
+import argparse
+import tensorflow as tf    
+from tensorflow.models.rnn import rnn, rnn_cell, seq2seq
+from tensorflow.models.rnn.rnn_cell import BasicRNNCell, BasicLSTMCell, LSTMCell, CWRNNCell
+import numpy as np
+import matplotlib.pylab as pl
+
+def get_seq_input_data(n_steps, batch_size, seq_width):
+    # seq_input_data = np.random.rand(n_steps, batch_size, seq_width).astype('float32')
+    seq_input_data = np.zeros((n_steps, batch_size, seq_width)).astype('float32')
+    seq_input_data[0, :, :] = 1.
+    seq_input_data[n_steps/2, :, :] = -1.
+    return seq_input_data
+
+# class seqrnn():
+#     outputs, states = seq2seq.rnn_decoder(inputs, self.initial_state, cell, loop_function=loop if infer else None, scope='rnnlm')
+
+class srnn():
+    def __init__(self, args):
+
+        self.size = args.rnn_size
+        self.n_steps = args.n_steps
+        self.batch_size = args.batch_size
+        self.input_dim = args.input_dim
+        self.num_layers = args.num_layers
+        
+        initializer = tf.random_uniform_initializer(-0.8,0.8)
+        # initializer = tf.zeros_initializer((size*2,1), dtype=tf.float32)
+
+        self.seq_input = tf.placeholder(tf.float32, [self.n_steps, self.batch_size, self.input_dim])
+        # sequence we will provide at runtime
+        self.early_stop = tf.placeholder(tf.int32)
+        # what timestep we want to stop at
+
+        self.inputs = [tf.reshape(i, (self.batch_size, self.input_dim)) for i in tf.split(0, self.n_steps, self.seq_input)]
+        # inputs for rnn needs to be a list, each item being a timestep. 
+        # we need to split our input into each timestep, and reshape it because split keeps dims by default
+        # result = tf.placeholder(tf.float32, [n_steps, batch_size, seq_width])
+        self.result = tf.placeholder(tf.float32, [None, self.input_dim])
+
+        if args.cell_type == "srnn":
+            cell = BasicRNNCell(self.size)#, seq_width, initializer=initializer)
+        elif args.cell_type == "lstm":
+            cell = BasicLSTMCell(self.size, forget_bias = 1.0)
+        elif args.cell_type == "lstmp":
+            cell = LSTMCell(self.size, self.input_dim, initializer=initializer) 
+        elif args.cell_type == "cw":
+            cell = CWRNNCell(self.size, [1, 4, 16, 64])#, seq_width, initializer=initializer)  
+
+        self.cell = cell = rnn_cell.MultiRNNCell([cell] * self.num_layers)
+        
+        # initial_state = cell.zero_state(batch_size, tf.float32)
+        self.initial_state = tf.random_uniform([self.batch_size, self.cell.state_size], -0.1, 0.1)
+
+        # self variables: scope RNN -> BasicRNNCell -> get_variable("Matrix", "Bias")
+        
+        # network type
+        if args.rnn_type == "rnn":
+            self.outputs, self.states = rnn.rnn(self.cell, self.inputs,
+                                                initial_state = self.initial_state,
+                                                sequence_length = self.early_stop)
+        elif args.rnn_type == "seq2seq":
+            self.outputs, self.states = seq2seq.rnn_decoder(self.inputs,
+                                                            self.initial_state,
+                                                            self.cell,
+                                                            loop_function=loop if False else None)
+            # set up lstm
+        self.final_state = self.states[-1]
+
+        self.W_o = tf.Variable(tf.random_normal([self.size,1], stddev=0.01))
+        self.b_o = tf.Variable(tf.random_normal([1], stddev=0.01))
+
+        print "type(outputs)", type(self.outputs)
+        self.output_cat = tf.reshape(tf.concat(1, self.outputs), [-1, self.size])
+        self.output = tf.nn.xw_plus_b(self.output_cat, self.W_o, self.b_o)
+        # self.final_state = states[-1]
+        self.output2 = tf.reshape(self.output, [self.batch_size, self.n_steps, self.input_dim])
+        self.output2 = self.output2 + tf.random_normal([self.batch_size, self.n_steps, self.input_dim], stddev=0.05)
+        # then transpose
+        self.output2 = tf.transpose(self.output2, [1, 0, 2])
+
+def run(model, args):
+    if model == None:
+        return
+    
+    iop = tf.initialize_all_variables()
+    # create initialize op, this needs to be run by the session!
+    session = tf.Session()
+    session.run(iop)
+    # actually initialize, if you don't do this you get errors about uninitialized stuff
+
+    seq_input_data = get_seq_input_data(args.n_steps, args.batch_size, args.input_dim)
+    
+    # prev_state = session.run(cell.zero_state(batch_size, tf.float32))
+    prev_state = session.run(tf.random_uniform([args.batch_size, model.cell.state_size], -1., 1.))
+
+    # check vars
+    # with tf.variable_scope(""):
+    # Wr = tf.get_variable("RNN/BasicRNNCell/Matrix")
+    tvars = tf.trainable_variables()
+    # print "trainable variables", tvars
+    for tvar in tvars:
+        print tvar.name
+        print session.run(tvar)
+
+    # print "current scope", session.run(tf.get_variable_scope())
+    tf.get_variable_scope().reuse_variables()
+    Wr = tf.get_variable("RNN/MultiRNNCell/Cell0/BasicRNNCell/Linear/Matrix")
+    session.run(Wr.assign(Wr + 0.1))
+    print "Wr", session.run(Wr)
+        
+
+    allouts = []
+    allstates = []
+    allhiddens = []
+    for i in range(3):
+        print "pstate", prev_state
+        feed = {model.early_stop: args.n_steps,
+                model.seq_input: seq_input_data,
+                model.initial_state: prev_state}
+        # feed = {early_stop:n_steps, seq_input: seq_input_data}
+        # define our feeds. 
+        # early_stop can be varied, but seq_input needs to match the shape that was defined earlier
+
+        outs, fstate, hidden = session.run([model.output, model.final_state, model.output_cat], feed_dict=feed)
+        print "session return types", type(outs), type(fstate), type(hidden)
+        prev_state = fstate
+        print "fstate", fstate
+        allouts.append(outs)
+        allstates.append(fstate)
+        allhiddens.append(hidden)
+    
+
+    # Wr_ = session.run(Wr)
+    # print Wr_
+    
+    # run once
+    # output is a list, each item being a single timestep. Items at t>early_stop are all 0s
+    # print outs
+    print type(outs)
+    print len(outs)
+    print type(outs[0])
+    print outs[0].shape
+    print "allouts", len(allouts)
+
+    pl.subplot(411)
+    pl.plot(seq_input_data[:,0,:])
+    for i,out in enumerate(allouts):
+        print out.shape
+        pl.subplot(412)
+        pl.plot(out)
+        pl.subplot(413)
+        pl.plot(allstates[i])
+        pl.subplot(414)
+        print hidden[i].shape
+        pl.plot(allhiddens[i])
+    # pl.plot(outs)
+    pl.show()
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--rnn_size", type=int, default=4)
+    parser.add_argument("--rnn_type", type=str, default="rnn", help="Either: rnn or seq2seq")
+    parser.add_argument("--cell_type", type=str, default="srnn", help="rnn, gru, lstm, lstmp, cw")
+    parser.add_argument("--batch_size", type=int, default=1)
+    parser.add_argument("--n_steps", type=int, default=200)
+    parser.add_argument("--input_dim", type=int, default=2)
+    parser.add_argument("--num_layers", type=int, default=1)
+
+    args = parser.parse_args()
+    
+    np.random.seed(1)      
+    # size = 16
+    # batch_size= 1 # 100
+    # n_steps = 200
+    # input_dim = 1
+
+    model = srnn(args)
+    run(model, args)
